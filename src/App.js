43ab85d79634a6e5a514b446b545d22ef3e8752c
stella-mary <mariastellamary@outlook.com>
2022-10-13 15:52:22 +0530

    Tour

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import Loading from './Loading'
+import Tours from './Tours'
+
+const url = 'https://course-api.com/react-tours-project'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [tours, setTours] = useState([])
+	const [loading, setLoading] = useState(true)
+
+	function removeTour(tourID) {
+		// add logic to update state for tour removal
+        setTours(tours.filter((tour) => {
+            return tour.id != tourID;
+        }))
+	}
+
+	async function fetchTours() {
+		// set loading to true here
+		setLoading(true)
+
+		const data = await fetch(url)
+		const jsonData = await data.json()
+		setTours(jsonData)
+
+		setLoading(false)
+		
+	}
+
+	useEffect(() => {
+		fetchTours()
+	}, [])
+
+	if (loading) {
+		return (
+			<main>
+				<Loading />
+			</main>
+		)
+	}
+
+	return (
+		<main>
+			<Tours tours={tours} removeTour={removeTour} />
+		</main>
+	)
 }
 
-export default App;
+export default App
